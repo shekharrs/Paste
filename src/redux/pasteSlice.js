a0aee89cdd94d92c17156709d38949e7ad9d4a35
shekhar <shekhart442@gmail.com>
2025-02-02 16:44:13 +0530

    updated

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,29 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
 const initialState = {
-    pastes: localStorage.getItem("pastes") ? JSON.parse(localStorage.getItem("pastes")) : []
-}
+  pastes: localStorage.getItem("pastes")
+    ? JSON.parse(localStorage.getItem("pastes"))
+    : [],
+};
 
 export const pasteSlice = createSlice({
-  name: 'paste',
+  name: "paste",
   initialState,
   reducers: {
     addToPastes: (state, action) => {
+      const paste = action.payload;
 
+      // ✅ Check if paste already exists
+
+      state.pastes.push(paste);
+      localStorage.setItem("pastes", JSON.stringify(state.pastes));
+      toast("Paste Created Successfully!");
     },
-    updateToPastes: (state, action) => {
-
-    },
-    resetAllPastes: (state, action) => {
-
-    },
-    removeFromPastes: (state, action) => {
-
-    }
+    updateToPastes: (state, action) => {},
+    resetAllPastes: (state, action) => {},
+    removeFromPastes: (state, action) => {},
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes } = pasteSlice.actions
+export const { addToPastes, updateToPastes, resetAllPastes, removeFromPastes } =
+  pasteSlice.actions;
 
-export default pasteSlice.reducer
+export default pasteSlice.reducer;
