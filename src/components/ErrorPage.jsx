de39a4678a49c0522b1358fa320736c626d03f73
shekhar <shekhart442@gmail.com>
2025-02-03 14:21:00 +0530

    Updated Error Page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorPage = () => {
   return (
     <div>
-        <h2>Unexpected Application Error!
+        <h2 className='mt-60 text-2xl font-extralight text-red-400'>Unexpected Application Error!
         404 Not Found😑</h2>
     </div>
   )
